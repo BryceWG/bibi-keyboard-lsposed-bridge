d21ebc9d30dbfde46c59cf11c7a6f97d425a8e25
BryceWG <zhang1577006721@qq.com>
2026-08-02 12:21:08 +0800

    chore(release): Bump version to 0.3.2 (versionCode 21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.brycewg.asrkb.imebridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.3.1"
+        versionCode = 21
+        versionName = "0.3.2"
     }
 
     signingConfigs {
